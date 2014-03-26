25580752d0c9ec4a63924543596a1da17baeca76
gianlucamateo <gianluca.mateo@gmail.com>
2014-03-26 09:58:05 +0100

    Added files for histogram-based color pruning

---
+++ KADA/Histogram.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using Color = System.Drawing.Color;
+using Microsoft.Xna.Framework;
+
+namespace HistogramReader
+{
+    class Histogram
+    {
+        public float[, ,] colorBins;
+        private readonly int RESOLUTION;
+        private readonly int BINSIZE;
+
+        private float threshold;
+        public Histogram(Bitmap image, float threshold, int resolution)
+        {
+            this.RESOLUTION = resolution;
+            BINSIZE = 256 / RESOLUTION;
+            colorBins = new float[RESOLUTION,RESOLUTION,RESOLUTION];
+            this.threshold = threshold;
+            this.generate(image);
+        }
+
+        public void generate(Bitmap image)
+        {
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    Color c = image.GetPixel(x, y);
+
+                    
+                    if (!c.IsEmpty)
+                    {
+                        int r = c.R / BINSIZE;
+                        int g = c.G / BINSIZE;
+                        int b = c.B / BINSIZE;
+                        colorBins[r, g, b]++;
+                    }
+
+                }
+            }
+            this.colorBins[RESOLUTION - 1, RESOLUTION - 1, RESOLUTION - 1] = 0;
+            this.colorBins[0, 0, 0] = 0;
+            /*float green = 0, red = 0, blue = 0;
+            for (int i = 0; i < 255; i++)
+            {
+                g[i] = (float)Math.Log10(g[i]);
+                r[i] = (float)Math.Log10(r[i]);
+                b[i] = (float)Math.Log10(b[i]);
+            }
+
+            for (int i = 0; i < 255; i++)
+            {
+                if (g[i] > 0)
+                    green += g[i];
+                if (r[i] > 0)
+                    red += r[i];
+                if (b[i] > 0)
+                    blue += b[i];
+            }*/
+
+            /*for (int i = 0; i < 255; i++)
+            {
+                g[i] /= green;
+                r[i] /= red;
+                b[i] /= blue;
+            }*/
+        }
+
+        public float getValue(Vector3 color)
+        {
+            if (color.Length() < 50)
+                return 0;
+            if (color.Length() > 400)
+                return 0;
+
+
+            /*float green = g[(int)color.Y];
+            float red = r[(int)color.X];
+            float blue = r[(int)color.Z];*/
+            int r = (int)color.X / BINSIZE;
+            int g = (int)color.Y / BINSIZE;
+            int b = (int)color.Z / BINSIZE;
+
+            //bool isbrick = green > threshold && red > threshold && blue > threshold;
+            bool isbrick = colorBins[r, g, b] > threshold;
+
+            if (isbrick)
+                return 1;
+            /*sum += r[(int)color.X];
+            sum += g[(int)color.Y];
+            sum += b[(int)color.Z];
+            if(sum>0.01f)
+                return 1;*/
+            return 0;
+
+        }
+    }
+}
---
+++ KADA/HistogramReader.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading;
+using System.Collections;
+using Microsoft.Xna.Framework;
+using Color = System.Drawing.Color;
+
+namespace HistogramReader
+{
+    class HistogramReader
+    {
+        static int Main(String[] args)
+        {
+            List<Histogram> histograms = new List<Histogram>();
+
+            Bitmap red = (Bitmap)Image.FromFile("../../Red_cleaned_filled.png", true);
+            Histogram r = new Histogram(red, 150, 8);
+            histograms.Add(r);
+
+            Bitmap green = (Bitmap)Image.FromFile("../../Green_cleaned_filled.png", true);
+            Histogram g = new Histogram(green, 25, 16);
+            histograms.Add(g);
+
+            Bitmap blue = (Bitmap)Image.FromFile("../../Blue_cleaned_filled.png", true);
+            Histogram b = new Histogram(blue,100, 8);
+            histograms.Add(b);
+
+            Bitmap yellow = (Bitmap)Image.FromFile("../../Yellow_cleaned_filled.png", true);
+            Histogram ye = new Histogram(yellow, 150, 8);
+            histograms.Add(ye);
+
+
+            Bitmap test = (Bitmap)Image.FromFile("../../test_against.png", true);
+
+
+            for (int x = 0; x < test.Width; x++)
+            {
+                for (int y = 0; y < test.Height; y++)
+                {
+                    Color c = test.GetPixel(x, y);
+                    Vector3 color = new Vector3(c.R, c.G, c.B);
+                    float maxVal = 0;
+                    foreach (Histogram h in histograms)
+                    {
+                        float val = h.getValue(color);
+                        if (val > maxVal)
+                        {
+                            maxVal = val;
+                        }
+                    }
+                    maxVal *= 255;
+                    if (maxVal !=0)
+                        test.SetPixel(x, y, Color.FromArgb(255, (int)maxVal, 0, (int)maxVal));
+                }
+            }
+            test.Save("../../test_against_output.png");
+
+
+            return 0;
+        }
+    }
+}
